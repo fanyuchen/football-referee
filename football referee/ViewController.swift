a1b67f240c83e92abc78079b3ccadc2e88d96a32
fanyuchen <fanyuchen@Mac02.local>
2016-04-14 12:01:56 +0800

    足球

---
+++ football referee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  football referee
+//
+//  Created by 范宇辰 on 16/3/28.
+//  Copyright © 2016年 范宇辰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
